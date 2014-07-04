65067d506682b7ede4ef26804c0b0a7db0b81e78
Menoc <m3nn0c@gmail.com>
2014-07-04 20:47:46 +0300

    Menus
    Adding main menu and high scores

---
+++ high_score.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class high_score : MonoBehaviour {
+
+	public float mainboxh;
+	public float mainboxw;
+
+	public float box_title;
+	public float boxpos;
+
+
+	public float boxh;
+	
+	public float topBannerH;
+	public float topBannerW;
+	
+	public float buttonSizeH;
+	public float buttonSizeW;
+	public float buttonPos1;
+	public float buttonPos2;
+	public float buttonPos3;
+	
+	public float bottomBannerH;
+	public float bottomBannerW;
+	public float bottomBannerPos;
+	int a=0;
+	
+	
+	void Start () {
+
+		mainboxh = Screen.height - Screen.height*1/3;
+		mainboxw = Screen.width*2/3;
+		boxpos = 0f;
+
+		boxh = Screen.height / 8;
+		
+	}
+	
+	void OnGUI() {
+		
+		//GUI.Box(new Rect(Screen.width/6,boxpos,mainboxw,mainboxh),"Place for Banner\nor ads, possibly");
+		GUI.Box(new Rect(Screen.width/6,boxpos,mainboxw,boxh),"High scores:");
+		GUI.Box(new Rect(Screen.width/6,boxpos+boxh+2,mainboxw,boxh),"Name 1:\t"+a);
+		GUI.Box(new Rect(Screen.width/6,boxpos+(boxh+2)*2,mainboxw,boxh),"Name 2:\t"+a);
+		GUI.Box(new Rect(Screen.width/6,boxpos+(boxh+2)*3,mainboxw,boxh),"Name 3:\t"+a);
+		GUI.Box(new Rect(Screen.width/6,boxpos+(boxh+2)*4,mainboxw,boxh),"Name 4:\t"+a);
+		GUI.Box(new Rect(Screen.width/6,boxpos+(boxh+2)*5,mainboxw,boxh),"Name 5:\t"+a);
+		GUI.Box(new Rect(0,boxpos+(boxh+2)*6,Screen.width,Screen.height*2/3),"one more ads block");
+	}
+}
---
+++ main_menu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+
+
+
+public class main_menu : MonoBehaviour {
+
+
+
+	public float topBannerH;
+	public float topBannerW;
+
+	public float buttonSizeH;
+	public float buttonSizeW;
+	public float buttonPos1;
+	public float buttonPos2;
+	public float buttonPos3;
+
+	public float bottomBannerH;
+	public float bottomBannerW;
+	public float bottomBannerPos;
+	
+	
+
+	void Start () {
+		topBannerH = Screen.height/4;
+		topBannerW = Screen.width;
+		buttonSizeH = Screen.height/10;
+		buttonSizeW = Screen.width/2;
+		buttonPos1 = topBannerH;
+		buttonPos2 = topBannerH+buttonSizeH+10;
+		buttonPos3 = topBannerH+(buttonSizeH+10)*2;
+		bottomBannerH = Screen.height/4;
+		bottomBannerW =  Screen.width;
+		bottomBannerPos = topBannerH+buttonSizeH*5;	
+	
+	}
+	
+	void OnGUI() {
+
+		GUI.Box(new Rect(0,0,topBannerW,topBannerH),"Place for Banner\nor ads, possibly");
+		
+
+		if (GUI.Button(new Rect(buttonSizeW/2,buttonPos1+5,buttonSizeW,buttonSizeH),"Play")){
+			Application.LoadLevel("main_scene");
+		}
+
+		if (GUI.Button(new Rect(buttonSizeW/2,buttonPos2+5,buttonSizeW,buttonSizeH),"High Score")){
+		}
+
+		if (GUI.Button(new Rect(buttonSizeW/2,buttonPos3+5,buttonSizeW,buttonSizeH),"Help")){
+		}
+
+
+		GUI.Box(new Rect(0,bottomBannerPos,bottomBannerW,bottomBannerH),"Moar ads!");
+	}
+}
